167492e149fdf44dd1296b422656eb11bdfc8614
Emily <echeun17@mail.ccsf.edu>
2019-08-29 17:15:52 -0700

    show img on createPost

---
+++ public/javascripts/show-img.js
@@ -0,0 +1,13 @@
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            console.log(e.target.result);
+            $('#img-place')
+                .attr('style', `background-image: url('${e.target.result}'); background-size: cover; background-repeat: no-repeat;`);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
