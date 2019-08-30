38f302cbdc59f6b1766cec4f293ee5865c8e8b07
Emily <echeun17@mail.ccsf.edu>
2019-08-29 22:29:09 -0700

    test file-upload

--- public/javascripts/show-img.js
+++ public/javascripts/show-img.js
@@ -1,24 +1,13 @@
-function previewFile() {
-  
-       var file = $( "input:file" ).files[0];
+function readURL(input) {
+    if (input.files && input.files[0]) {
         var reader = new FileReader();
 
-        reader.addEventListener("load", function () {
-            $('#img-place').attr('style', `background-image: url('${reader.result}'); background-size: cover; background-repeat: no-repeat;`);
-          }, false);
-        
-          if (file) {
-            reader.readAsDataURL(file);
-          }
-/*
         reader.onload = function (e) {
             console.log(e.target.result);
             $('#img-place')
-                .attr('style', `background-image: url('${e.target.result}'); background-size: cover; background-repeat: no-repeat;`);
+                .attr('src', e.target.result);
         };
-        $( "input:file" )
 
         reader.readAsDataURL(input.files[0]);
-        */
-    
+    }
 }
